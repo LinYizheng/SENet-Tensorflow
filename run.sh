98facf3657527ae1a6c47cdf0b526b0c6fa0c0c7
LinYizheng <40623566+LinYizheng@users.noreply.github.com>
2019-01-25 03:47:06 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,28 +1,28 @@
 host=$(hostname -s)
-if [ ${host} == "citthdp1" ]
+if [ ${host} == "hostname1" ]
 then
-	echo "doCommand on citthdp1"
+	echo "doCommand on hostname1"
 	lsof -i:2222 | awk '{if (NR>1){print $2}}' | xargs kill -9
-	python /home/hadoop/pythonw/Sunny/distributed_tf/distribute.py --job_name=ps --task_index=0
+	python /home/hadoop/pythonw/Sunny/distributed_tf/distributed_SEnet.py --job_name=ps --task_index=0
 
-elif [ ${host} == "citthdp2" ]
+elif [ ${host} == "hostname2" ]
 then
-	echo "doCommand on citthdp2"
+	echo "doCommand on hostname2"
 	lsof -i:2224 | awk '{if (NR>1){print $2}}' | xargs kill -9
 	#ssh hadoop@${host} "python /home/hadoop/pythonw/Sunny/distributed_tf/distribute.py --job_name=worker --task_index=1"
-	python /home/hadoop/pythonw/Sunny/distributed_tf/distribute.py --job_name=worker --task_index=0
+	python /home/hadoop/pythonw/Sunny/distributed_tf/distributed_SEnet.py --job_name=worker --task_index=0
 
-elif [ ${host} == "citthdp3" ]
+elif [ ${host} == "hostname3" ]
 then
-	echo "doCommand on citthdp3"
+	echo "doCommand on hostname3"
 	lsof -i:2224 | awk '{if (NR>1){print $2}}' | xargs kill -9
 	#ssh hadoop@${host} "python /home/hadoop/pythonw/Sunny/distributed_tf/distribute.py --job_name=worker --task_index=2"
-	python /home/hadoop/pythonw/Sunny/distributed_tf/distribute.py --job_name=worker --task_index=1
+	python /home/hadoop/pythonw/Sunny/distributed_tf/distributed_SEnet.py --job_name=worker --task_index=1
 
-elif [ ${host} == "citthdp4" ]
+elif [ ${host} == "hostname4" ]
 then
-	echo "doCommand on citthdp4"
+	echo "doCommand on hostname4"
 	lsof -i:2224 | awk '{if (NR>1){print $2}}' | xargs kill -9
 	#ssh hadoop@${host} "python /home/hadoop/pythonw/Sunny/distributed_tf/distribute.py --job_name=worker --task_index=3"
-	python /home/hadoop/pythonw/Sunny/distributed_tf/distribute.py --job_name=worker --task_index=2
+	python /home/hadoop/pythonw/Sunny/distributed_tf/distributed_SEnet.py --job_name=worker --task_index=2
 fi
